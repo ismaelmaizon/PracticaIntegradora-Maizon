39660bbae0e0ad3fabaa7f5ba7a3dc6cf26e89c0
cristian maizon <ismael.maizon1234@gmail.com>
2023-10-18 22:10:02 -0300

    testing

---
+++ src/test/carts.test.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import supertest from 'supertest';
+
+
+
+const expect = chai.expect;
+const requester = supertest('http://localhost:8080')
+
+
+describe('test', () => {
+    describe('test carts', () => {
+        it('Get /api/products/:pid debe traer la informacion de un producto', async () => {
+            let id = '649dcbddced4931906e71ed5';
+            const result = await requester
+                .get(`/api/products/${id}`)
+            console.log(result.statusCode, result._body);
+            let body = result._body
+            expect(result.statusCode).to.be.equal(200)
+            expect(body.product).to.have.property('_id')
+            expect(result.ok).to.be.ok
+        })
+        it('Get /api/products/:pid debe traer la informacion de un producto', async () => {
+            let id = '649dcbddced4931906e71ed5';
+            const result = await requester
+                .get(`/api/products/${id}`)
+            console.log(result.statusCode, result._body);
+            let body = result._body
+            expect(result.statusCode).to.be.equal(200)
+            expect(body.product).to.have.property('_id')
+            expect(result.ok).to.be.ok
+        })
+        it('Get /api/products/:pid debe traer la informacion de un producto', async () => {
+            let id = '649dcbddced4931906e71ed5';
+            const result = await requester
+                .get(`/api/products/${id}`)
+            console.log(result.statusCode, result._body);
+            let body = result._body
+            expect(result.statusCode).to.be.equal(200)
+            expect(body.product).to.have.property('_id')
+            expect(result.ok).to.be.ok
+        })
+    })
+})
